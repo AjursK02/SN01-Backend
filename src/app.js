f9bbe4bd9c305bbafdce9af48141a3aca6a666db
Bandana858 <bandanar858@gmail.com>
2025-07-02 13:38:42 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("starting a new project")
+
+const express = require("express")
+
+const app = express();
+
+
+
+app.use('/hello', (req, res)=>{
+    res.send("hello hello hello hello hello")
+})
+
+app.use('/test', (req, res)=>{
+    res.send("hello from the test server")
+})
+
+app.use('/', (req, res)=>{
+    res.send("hello from the server......")
+})
+
+app.listen(3000, ()=>{
+    console.log("server is successfully listening on port 3000...")
+});
